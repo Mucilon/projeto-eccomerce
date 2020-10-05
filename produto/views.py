55c5c5c0276108f364144e1054bc3b24476e131f
Mucilon <mucilonws@gmail.com>
2020-10-05 18:23:59 -0300

    Detalhes dos Produtos(Templates e Views)

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from .models import Produto
 
@@ -9,8 +10,11 @@ class ListaProdutos(ListView):
     context_object_name = 'produtos'
     paginate_by = 5
 
-class DetalheProduto(View):
-    pass
+class DetalheProduto(DetailView):
+    model = Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
     pass
