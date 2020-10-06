28c4e75602658f22f46a0feae6af91fb1bf30766
Mucilon <mucilonws@gmail.com>
2020-10-06 20:58:59 -0300

    Trabalhando com Sessions(Adicionar ao carrinho(Parte 1))

--- produto/views.py
+++ produto/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.http import HttpResponse
+from django.contrib import messages
 from django.views import View
-from .models import Produto
+from .models import Produto, Variacao
 
 class ListaProdutos(ListView):
     model = Produto
@@ -17,7 +19,30 @@ class DetalheProduto(DetailView):
     slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
-    pass
+    def get(self, *args, **kwargs):
+        http_referer = self.request.META.get('HTTP_REFERER',reverse('produto:lista'))
+        variacao_id = self.request.GET.get('vid')
+
+        if not variacao_id:
+            messages.error(self.request,'Produto não existe')
+            return render(http_referer)
+
+        variacao = get_object_or_404(Variacao, id=variacao_id)
+
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+
+        carrinho = self.request.session['carrinho']
+
+        if variacao_id in carrinho:
+            # TODO: Variacao existe no carrinho
+            pass
+        else:
+            # TODO: Variacao não existe no carrinho
+            pass
+
+        return HttpResponse('Adicionar ao Carrinho')
 
 class RemoverDoCarrinho(View):
     pass
